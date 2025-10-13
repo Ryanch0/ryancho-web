cc81bbd6d45d2e9796b9871d76dffce3ed5df626
ryanch0 <tjddnfkdls0@naver.com>
2025-10-13 23:59:36 +0900

    feat: Prevent unauthorized users from accessing protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,22 @@
-//TODO AUTH MIDDLEWARE
+import { createClientForServer } from '@/lib/supabase/server'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
-export const middleware = () => {}
+export async function middleware(request: NextRequest) {
+  const response = NextResponse.next({ request })
+
+  const supabase = await createClientForServer()
+
+  const data = await supabase.auth.getSession()
+  const session = data.data.session
+
+  if (!session) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  return response
+}
+
+export const config = {
+  matcher: ['/write/:path*']
+}
