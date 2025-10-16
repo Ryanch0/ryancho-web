f805ea3f231defedc0d160feafc68846c7790ab5
ryanch0 <tjddnfkdls0@naver.com>
2025-10-16 14:49:39 +0900

    feat: Add MarkDownTOC.tsx

---
+++ src/features/markdown/components/MarkDownTOC.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import useMarkDownToc from '@/features/markdown/hooks/useMarkDownToc'
+import Link from 'next/link'
+
+const MarkDownTOC = () => {
+  const { toc } = useMarkDownToc()
+
+  return (
+    <ul>
+      <h2>TOC</h2>
+      {toc.map(({ key, value, headTag }) => {
+        return (
+          <li key={`${headTag}-${key}`}>
+            <Link href={`#${key}`} className={'text-blue-500'}>
+              {value}
+            </Link>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+export default MarkDownTOC
--- src/features/posts/components/PostDetail.tsx
+++ src/features/posts/components/PostDetail.tsx
@@ -1,15 +1,17 @@
+import MarkDownTOC from '@/features/markdown/components/MarkDownTOC'
 import renderMdxComponents from '@/features/markdown/components/renderMdxComponents'
 import { MDXRemote } from 'next-mdx-remote-client/rsc'
 
 type Props = {
   content: string
 }
-const PostDetail = async ({ content }: Props) => {
+const PostDetail = ({ content }: Props) => {
   const components = renderMdxComponents()
 
   return (
     <div className="prose">
       <MDXRemote source={content} components={components} />
+      <MarkDownTOC />
     </div>
   )
 }
