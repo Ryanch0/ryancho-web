0b5eb517a266ce6b0d644dd48984f596454d05e6
ryanch0 <tjddnfkdls0@naver.com>
2025-10-20 10:57:11 +0900

    feat: Allow Next.js to import images from Supabase storage

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import removeImports from 'next-remove-imports'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_SUPABASE_STORAGE_HOST!,
+        pathname: process.env.NEXT_PUBLIC_SUPABASE_STORAGE_PATHNAME
+      }
+    ]
+  }
 }
 
 export default removeImports({})(nextConfig)
