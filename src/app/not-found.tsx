958e3d557e5aaae4f189a930901f20efd3821ac2
ryanch0 <tjddnfkdls0@naver.com>
2025-11-03 16:45:58 +0900

    feat: Add styling to not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,23 @@
+import Footer from '@/shared/components/Footer'
+import Header from '@/shared/components/Header/components/Header'
+import MainLink from '@/shared/components/MainLink'
+
 const NotFound = () => {
-  return <div>Not found</div>
+  return (
+    <main className="layout">
+      <Header />
+      <div className={'flex flex-col gap-30 pt-18'}>
+        <div>
+          <h2 className={'title-style'}>404</h2>
+          <div className={'second-font-style py-1'}>
+            <p>Page Not Found</p>
+            <span>by</span> <MainLink />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </main>
+  )
 }
 
 export default NotFound
