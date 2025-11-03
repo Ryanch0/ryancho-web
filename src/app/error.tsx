517c703b7b3b4454048dbb04f935b6db863f7ee2
ryanch0 <tjddnfkdls0@naver.com>
2025-11-03 16:57:20 +0900

    feat: Add error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,17 @@
 'use client'
 
-const Error = () => {
-  return <>ERROR PAGE</>
-}
+import MainLink from '@/shared/components/MainLink'
 
-export default Error
+export default function Error() {
+  return (
+    <main className="layout">
+      <div className={'pt-18'}>
+        <h2 className="title-style">Error</h2>
+        <div className="second-font-style py-1">
+          <p>Something went wrong</p>
+        </div>
+        <MainLink title={<h4>Return Home</h4>} />
+      </div>
+    </main>
+  )
+}
